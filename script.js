eb9be107a0fee3be7d993391d8ffd888406bebbf
Gregory Barrett <gbarrett15@gmail.com>
2017-07-17 16:40:01 +0100

    grid made

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+for (var i =0; i <256; i++){
+var $div ='<div></div>';
+$("body").append($div);
+}
+});
+/*$(document).ready(function(){
+  $("div").on('click', function(){
+    $(this).toggleClass();
+  });
+});
+*/
